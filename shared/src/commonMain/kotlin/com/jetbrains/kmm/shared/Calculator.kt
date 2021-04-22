da0e77daa19aac1314030f1e57c6e599a71948ab
gioPucela <sergio.sastre.florez@gmail.com>
2021-04-22 15:37:29 +0200

    Add IBAN String formatter

--- shared/src/commonMain/kotlin/com/jetbrains/kmm/shared/Calculator.kt
+++ shared/src/commonMain/kotlin/com/jetbrains/kmm/shared/Calculator.kt
@@ -3,5 +3,16 @@ package com.jetbrains.kmm.shared
 class Calculator {
     companion object {
         fun sum(a: Int, b: Int): Int = a + b
+
+        fun getFormattedIban(ibanAsString: String): String = getFormattedString(ibanAsString, formatWith = " ", formatSpacing = 4)
+
+        private fun getFormattedString(ibanAsString: String, formatWith: String, formatSpacing: Int): String {
+            val formatterIban = StringBuilder()
+            ibanAsString.replace("\\s".toRegex(), "").mapIndexed { index: Int, char: Char ->
+                val newSequence: String = if (index % formatSpacing == 0) "$formatWith$char" else char.toString()
+                formatterIban.append(newSequence)
+            }
+            return formatterIban.trim().toString()
+        }
     }
 }
